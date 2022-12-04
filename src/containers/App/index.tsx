9eb22d17bae77d970657615f2186a92944e5c5d8
mikey1384 <mikey1384@gmail.com>
2022-12-04 22:08:39 +0700

    more updates

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,19 +1,8 @@
-import { mobileMaxWidth } from '~/constants/css';
-import { css } from '@emotion/css';
 import ErrorBoundary from '~/components/ErrorBoundary';
 
 export default function App() {
   return (
-    <ErrorBoundary
-      componentPath="App/index"
-      className={css`
-        height: CALC(100% - 4.5rem);
-        width: 100%;
-        @media (max-width: ${mobileMaxWidth}) {
-          height: 100%;
-        }
-      `}
-    >
+    <ErrorBoundary componentPath="App/index">
       <div id="App">this is where everything begins</div>
     </ErrorBoundary>
   );
