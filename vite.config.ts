dc9930e1e56ebb4337b854760a4e47955feb1507
mikey1384 <mikey1384@gmail.com>
2022-12-04 09:57:20 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
@@ -24,11 +23,5 @@ export default defineConfig({
   define: {
     global: 'window',
     'process.env': {}
-  },
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      '~': resolve(__dirname, 'src')
-    }
   }
 });
